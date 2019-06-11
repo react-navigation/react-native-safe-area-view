e6a1a4221a80bf90b048538d05c86650027e0b6f
Evan Bacon <baconbrix@gmail.com>
2019-06-11 09:37:40 -0700

    Updated example to 33

--- example/index.js
+++
@@ -1,1 +0,0 @@
-import "node_modules/expo/AppEntry.js";
--- example/index.web.js
+++
@@ -1,5 +0,0 @@
-import App from './App';
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<App exp={{}} />, global.document.getElementById('root'));
