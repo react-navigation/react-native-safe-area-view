9d0ffc736698df54cd744e6fe8b00db041119027
Bruno Lemos <brunohplemos@gmail.com>
2019-08-29 11:23:24 -0300

    Add web exports for react-native-safe-area-context
    Fix #88

--- src/index.web.js
+++ src/index.web.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { EdgeInsets } from 'react-native-safe-area-context';
+
+const defaultInsets: EdgeInsets = { top: 0, bottom: 0, left: 0, right: 0 };
+
+export const SafeAreaContext = React.createContext<EdgeInsets | null>(
+  defaultInsets,
+);
+
+export const SafeAreaConsumer = SafeAreaContext.Consumer;
+export const SafeAreaProvider = SafeAreaContext.Provider;
+
+export function useSafeArea(): EdgeInsets {
+  return defaultInsets
+};
+
 export default View;
