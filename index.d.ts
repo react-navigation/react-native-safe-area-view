38fb858e1767bdf7f57f35af8ecd8f14abe5bd31
Xianming Zhong <chinesedfan@qq.com>
2019-09-08 18:28:33 +0800

    Add TS definitions of setStatusBarHeight and setStatusBarHidden

--- index.d.ts
+++ index.d.ts
@@ -21,7 +21,10 @@ export const getInset: (
   isLandscape?: boolean
 ) => number;
 
-export const SafeAreaView: ComponentClass<SafeAreaViewProps>;
+export const SafeAreaView: ComponentClass<SafeAreaViewProps> & {
+  setStatusBarHeight: (height: number) => void;
+  setStatusBarHidden: (hidden: boolean) => void;
+};
 
 export const withSafeArea: <P extends object>(
   safeAreaViewProps?: SafeAreaViewProps
