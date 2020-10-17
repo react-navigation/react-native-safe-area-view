219255a5bbb6850ca889d8bd841083b05c07ffb0
minhnguyenwp <minhnguyenwp@gmail.com>
2020-10-17 11:41:45 +0700

    revert 0.12

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ class SafeView extends Component {
     const WIDTH = isLandscape ? X_HEIGHT : X_WIDTH;
     const HEIGHT = isLandscape ? X_WIDTH : X_HEIGHT;
 
-    this.view._component.measureInWindow((winX, winY, winWidth, winHeight) => {
+    this.view.getNode().measureInWindow((winX, winY, winWidth, winHeight) => {
       if (!this.view) {
         return;
       }
